c61bc031453813a66c2489533e19b0d052d97924
Wagh-Pradnyesh <pradnyeshwagh@gmail.com>
2026-01-13 14:09:18 +0530

    Add static request and response handle

--- SpringDemo/src/main/java/com/infogalaxy/springdemo/controller/EmployeeController.java
+++ SpringDemo/src/main/java/com/infogalaxy/springdemo/controller/EmployeeController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.infogalaxy.springdemo.entity.EmpEntity;
+
 @RestController
 @RequestMapping("/empapi")
 public class EmployeeController {
@@ -18,6 +20,19 @@ public class EmployeeController {
 	{
 		return "This Is Your Emp ID";
 	}
+	
+	
+	@GetMapping("/getemp")
+	public EmpEntity getEmp()
+	{
+		EmpEntity empEntity = new EmpEntity();
+		empEntity.setId(101);
+		empEntity.setName("Nanesh");
+		empEntity.setEmail("nanesh@gmail.com");
+		empEntity.setMobno("1122334455");
+		return empEntity;
+	}
+	
  
 	
 }
