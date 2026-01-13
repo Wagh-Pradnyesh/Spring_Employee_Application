cc3ff1dc88d1d9085dbb734ac6ba4517ee20f413
Wagh-Pradnyesh <pradnyeshwagh@gmail.com>
2026-01-13 16:17:21 +0530

    Use Array List

--- SpringDemo/src/main/java/com/infogalaxy/springdemo/controller/EmployeeController.java
+++ SpringDemo/src/main/java/com/infogalaxy/springdemo/controller/EmployeeController.java
@@ -1,5 +1,8 @@
 package com.infogalaxy.springdemo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,25 +15,38 @@ public class EmployeeController {
 	
 	@GetMapping
 	public String demo() {
-	return "Hii Web"+empId();	
+	return "Hii Emplyoee";	
 	}
 	
 	@GetMapping("/empid")
 	public String empId()
 	{
-		return "This Is Your Emp ID";
+		return demo()+"\n This Is Your Info\n"+getEmp();
 	}
 	
 	
 	@GetMapping("/getemp")
-	public EmpEntity getEmp()
+	public List<EmpEntity> getEmp()
 	{
+		List<EmpEntity> emplist = new ArrayList();
+		
+		
 		EmpEntity empEntity = new EmpEntity();
 		empEntity.setId(101);
-		empEntity.setName("Nanesh");
-		empEntity.setEmail("nanesh@gmail.com");
-		empEntity.setMobno("1122334455");
-		return empEntity;
+		empEntity.setName("\nNanesh");
+		empEntity.setEmail("\nnanesh@gmail.com");
+		empEntity.setMobno("\n1122334455");
+		
+		
+		EmpEntity empEntity2 = new EmpEntity();
+		empEntity2.setId(102);
+		empEntity2.setName("\nMahesh");
+		empEntity2.setEmail("\nMaHesh@gmail.com");
+		empEntity2.setMobno("\n1232334455");
+		
+		emplist.add(empEntity);
+		emplist.add(empEntity2);
+		return emplist;
 	}
 	
  
